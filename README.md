ebf28263f68c56221236824077c126c8f24256b5
paulhalleux <paulhalleux21@gmail.com>
2025-07-19 14:37:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Configurations
+
+The package provides the following configurations:
+- `@paulhalleux/eslint-config`: ESLint configuration for react projects.
+- `@paulhalleux/ts-config`: TypeScript configuration for projects.
+- `@paulhalleux/vite-config`: Vite configuration for projects.
+
+The following packages are also provided:
+- `@paulhalleux/json-schema-to-zod`: A tool to convert JSON Schema to Zod schemas.
---
+++ configs/vite-config/vite.lib.config.js
@@ -0,0 +1,38 @@
+import {resolve} from "path";
+
+import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import dts from "vite-plugin-dts";
+import eslint from "vite-plugin-eslint";
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint({ exclude: [/virtual:/, /node_modules/] }),
+    dts({
+      insertTypesEntry: true,
+    }),
+    tailwindcss()
+  ],
+  assetsInclude: ["/sb-preview/runtime.js"],
+  build: {
+    minify: true,
+    sourcemap: true,
+    lib: {
+      entry: resolve(process.cwd(), "src/index.ts"),
+      name: "index",
+      formats: ["es", "umd"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react-router-dom", "react-router"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
