1a5cec384dcfa61cb04ae2a2a3acc4c3836c6ec8
paulhalleux <paulhalleux21@gmail.com>
2025-07-23 21:01:47 +0200

    fix(timeline): track component memoization

--- packages/timeline/src/react/components/Track.tsx
+++ packages/timeline/src/react/components/Track.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useMemo } from "react";
 import { clsx } from "clsx";
+import isEqual from "lodash/isequal";
 
 import { TrackInstance } from "../../core/types.ts";
 
@@ -12,30 +13,27 @@ export type TrackProps = React.PropsWithChildren<{
 
 export const TrackProvider = React.createContext<TrackInstance | null>(null);
 
-export const Track = memo(function Track({
-  children,
-  track,
-  style,
-  className,
-  ...rest
-}: TrackProps) {
-  return (
-    <TrackProvider value={track}>
-      <div
-        style={useMemo(
-          () => ({
-            height: track.height,
-            top: track.top,
-            ...style,
-          }),
-          [style, track.height, track.top],
-        )}
-        className={clsx(styles.track, className)}
-        {...rest}
-        {...track.attributes}
-      >
-        {children}
-      </div>
-    </TrackProvider>
-  );
-});
+export const Track = memo(
+  function Track({ children, track, style, className, ...rest }: TrackProps) {
+    return (
+      <TrackProvider value={track}>
+        <div
+          style={useMemo(
+            () => ({
+              height: track.height,
+              top: track.top,
+              ...style,
+            }),
+            [style, track.height, track.top],
+          )}
+          className={clsx(styles.track, className)}
+          {...rest}
+          {...track.attributes}
+        >
+          {children}
+        </div>
+      </TrackProvider>
+    );
+  },
+  (prevProps, nextProps) => isEqual(prevProps, nextProps),
+);
